b231888ad073d96d526095b207074ccbdf4a0f28
frankemmy98 <emmyfrank349@gmail.com>
2022-11-06 15:13:57 -0600

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">About us</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam eius
+        consequatur placeat voluptatum iste corrupti excepturi eaque voluptates
+        delectus nulla suscipit assumenda hic earum voluptatem deleniti modi
+        quibusdam natus dolorum, id amet obcaecati? Omnis consectetur nulla
+        officiis nostrum voluptates architecto.
+      </p>
+    </section>
+  );
+};
+
+export default About;
